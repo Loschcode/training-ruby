68ea2eb55f9077fe5d7cceb4155e95efff189221
Loschcode <bonjour@laurentschaffner.com>
2016-04-07 20:25:10 +0200

    gemfile rspec

--- valid_parentheses.rb
+++ valid_parentheses.rb
@@ -2,13 +2,16 @@
 # Valid parenthesis check
 # by Loschcode
 #
+#
+require 'benchmark'
+
 def valid_parentheses(s)
   s.gsub!(/[^\(\)]/,"")
   (0..(s.size/2)).each { s.sub!('()', '') }
   s.empty?
 end
 
-check_parenthesis('()()()())()()()')
-check_parenthesis('()()()(())()()()')
-check_parenthesis(')()()()())()()()')
-check_parenthesis('()()(hey)())ho()()()')
+valid_parentheses('()()()())()()()')
+valid_parentheses('()()()(())()()()')
+valid_parentheses(')()()()())()()()')
+valid_parentheses('()()(hey)())ho()()()')

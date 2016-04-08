387e2e3e46854e6618744507ce5d4c90db3f2de0
Loschcode <bonjour@laurentschaffner.com>
2016-04-08 03:54:42 +0200

    valid phone number last version

--- valid_phone_number.rb
+++ valid_phone_number.rb
@@ -1,10 +1,14 @@
 #
 # Valid a US phone number
 # Bonus with !!() to force boolean
+# 
+# NOTE : Prefer `\A` and `\z` to `^` and `$`
+# Since it only checks a beginning of line in the second case
+# 
 # by Loschcode
 #
 def valid_phone_number(n)
-  !!(n =~ /^\(\d{3}\)\ \d{3}-\d{4}$/)
+  !!(n =~ /\A\(\d{3}\)\ \d{3}-\d{4}\z/)
 end
 
 valid_phone_number("(123) 456-7890")

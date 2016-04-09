e8c5b43892a4857c5782c3ce1b17a341c96c0ac5
Loschcode <bonjour@laurentschaffner.com>
2016-04-09 21:38:06 +0200

    is_triangle_number minor change

---
+++ guess_gifts.rb
@@ -0,0 +1,18 @@
+#
+# Guess the presents someone will get
+# by Loschcode
+#
+def guess_gifts(wishlist, presents)
+
+
+
+end
+
+guess_gifts([
+        {:name => "mini puzzle", :size => "small", :clatters => "yes", :weight => "light"},
+        {:name => "toy car", :size => "medium", :clatters => "a bit", :weight => "medium"},
+        {:name => "card game", :size => "small", :clatters => "no", :weight => "light"}
+        ],
+        
+        {size: "small", clatters: "no", weight: "light"}
+        )
--- is_triangle_number.rb
+++ is_triangle_number.rb
@@ -4,5 +4,5 @@
 #
 def is_triangle_number(n)
   return false unless n.is_a? Integer
-  (square = ((8*n)+1) ** 0.5) - square.to_i == 0
+  (sqrt = ((8*n)+1) ** 0.5) - sqrt.to_i == 0
 end
